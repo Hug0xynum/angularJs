6939de9728bd4f87077d384d5554f348ce49e5c1
Hug0xynum <hugo@oxynum.fr>
2017-12-19 20:21:54 +0100

    Initial commit for angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appsandboxApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appsandboxApp
+ */
+angular.module('appsandboxApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appsandboxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appsandboxApp
+ */
+angular.module('appsandboxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
